13a8d79d9ba2a7cbdbb4204f2e8153f1b8b92630
zzmeow0516 <99946679+zzmeow0516@users.noreply.github.com>
2022-04-08 22:58:56 +0800

    index change

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # study_data_structure
-学习数据结构的笔记~
+学习C语言和数据结构的笔记~
 
 
-book:<大话数据结构>溢彩加强版
+book:<大话数据结构>
+book:<C Primer Plus>
 
 
 Q1:为什么有debug.log文件会中断上传呢？
---
+++ codeforC/_2_easycprogram.c
@@ -0,0 +1,15 @@
+//an easy c program
+#include<stdio.h>
+
+void ring(void);
+
+int main(){
+    printf("s.b ring?\n");
+    ring();
+    printf("yes,it's me");
+    return 0;
+}
+
+void ring(void){
+    printf("i'm ringing...\n");
+}
